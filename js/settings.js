95a22e00dc702d76ae155dfc209f474b7f65a760
Kane Govaert <kanegovaert@protonmail.ch>
2018-10-24 13:58:39 +0200

    you can now save a master password

--- js/settings.js
+++ js/settings.js
@@ -1,28 +1,25 @@
 function saveOptions(e) {
+    console.log("aaa")
     e.preventDefault();
     browser.storage.sync.set({
         value: document.getElementById("#master-password").value
     });
-
-    console.log("dd"+document.getElementById("#master-password").value);
-
+    console.log(value);
+    console.log("aaa")
+    console.log("dd" + document.getElementById("#master-password").value);
 }
 
 function restoreOptions() {
-
     function setCurrentChoice(result) {
         console.log(result);
-
         document.getElementById("#master-password").value = result;
     }
 
     function onError(error) {
         console.log('Error: ' + error);
     }
-
     var getting = browser.storage.sync.get("value");
     getting.then(setCurrentChoice, onError);
 }
-
 document.addEventListener("DOMContentLoaded", restoreOptions);
 document.querySelector("form").addEventListener("submit", saveOptions);
